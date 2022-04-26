97552c9f2319e6b29d57f52784995df04aaa25d6
Thiel Serra <thiel.serra@gmail.com>
2022-04-26 19:35:24 -0300

    Instalando jetstream, livewire e rodando migrates necessárias

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,13 @@ use App\Http\Controllers\EventController;
 Route::get("/", [EventController::class, "index"]);
 Route::get("/events/create", [EventController::class, "create"]);
 Route::post("/events", [EventController::class, "store"]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
